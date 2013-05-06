d73abd788819bd09f69f218e792df11a4149ec3c
Abram Hindle <abram.hindle@softwareprocess.es>
2013-05-05 22:45:14 -0600

    first function

--- tutorial.hs
+++ tutorial.hs
@@ -2,3 +2,15 @@
 -- (C) 2013 Abram Hindle CC-BY 3.0
 -- For Edmonton Functional Programmers Users Group
 
+-- Hi this is a comment!
+
+-- Hi this is a function f which takes an argument x and returns 2 * x
+f x = 2 * x
+
+-- now load this file in ghci
+-- run: 
+--      f 2
+
+-- in ghci you can try new functions if you use the word 'let' before the function.
+-- let f x = 3 * x
+
